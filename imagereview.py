1eece355f0dd4bd0dbf56e5a19774e2eaa3ec70b
mike-sherman <wheelwrightmike@gmail.com>
2020-04-18 17:26:37 -0400

    revision 9 - added features
    * A button to create an animation of any days images with controlable framerate.
    * A progress bar for functions that can take some time.

--- imagereview.py
+++ imagereview.py
@@ -1,14 +1,4 @@
 # image review script   Nov 2019
-#
-# Copyright (c) 2019-2020 Michael Sherman. All rights reserved.
-#
-# LICENSE: LGPL
-# 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License (LGPL) as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
 # m.e.sherman
 # to make it quicker to review and delete images captured by my security cameras
 #
@@ -17,28 +7,23 @@
 #
 # imagereview6 released
 # imagereview7 - select folder always starts at base dir
-#              - released to public
-#
-# --------------------------------------------------
-#
-#  required to be filled in below:
-#   "/path/to/imagereview.ui"           - where the imagereview.ui file is located
-#   base_dir = '/path/to/images/'       - where the folders of images are located
-#   prog_dir = '/path/to/imagereview/'  - where the program data file is stored
-#
-# --------------------------------------------------
+# imagereview8 - add animate button to make a video of the current folder
+# imagereview9 - add progress bar
 
 import sys
 import os
 import json
+import shutil
+import fnmatch
+import subprocess
+import re
 
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt5 import uic, QtGui
 
 
-
-Ui_MainWindow, QtBaseClass = uic.loadUiType("/path/to/imagereview.ui")
+Ui_MainWindow, QtBaseClass = uic.loadUiType("/home/mike/Dropbox/src/python/imagereview/imagereview.ui")
 
             
             
@@ -47,11 +32,14 @@ class MyApp(QMainWindow, Ui_MainWindow):
     # class scope variables
     #
     # where the images are
-    base_dir = '/path/to/images/'
+    base_dir = '/path/to/images/'           # where image folders are
+    
     # where the program and datafile are
-    prog_dir = '/path/to/imagereview/'
+    prog_dir = '/path/to/imagereview/'      # where the program is
+    temp_dir = prog_dir + 'temp/'           # used for animations
+    
     # the name of the log file
-    log_file = prog_dir + "data.json"
+    log_file = prog_dir + "data.json"      # database of images
     input_dir = ''                          # the current driectory we are tagging
     prev_dir = ''                           # the previous direcotry in the directory list
     next_dir = ''                           # the next direcotry in the directory list
@@ -65,6 +53,9 @@ class MyApp(QMainWindow, Ui_MainWindow):
     data = []                               # local log data
     dataupdate = []                         # list of to-be-removed
     changes = 0                             # have we changed anything?
+    fps = 15                                # animation frames per second
+    percentstep = 1
+    percentdone = 0
     
     # class init
     def __init__(self):
@@ -74,6 +65,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
         logo_src = "appLogo.png"
         self.setWindowIcon(QtGui.QIcon(logo_src))
         self.ui.folderLabel.setText(self.base_dir)
+        self.ui.fpsLabel.setText(str(self.fps) + " fps")
+        self.ui.progress.setValue(self.percentdone)
         
         self.ui.folderButton.clicked.connect(self.choose_directory)
         self.ui.folderButton.setToolTip('Choose directory')
@@ -92,14 +85,70 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.ui.prevfolderButton.clicked.connect(self.prevfolder)
         self.ui.tognextButton.clicked.connect(self.keepnextimage)
         self.ui.filelistWidget.itemClicked.connect(self.itemclicked)
+        self.ui.animate.clicked.connect(self.animate)
+        self.ui.fpsSlider.valueChanged.connect(self.set_framerate)
+        
+#        self.ui.testButton.clicked.connect(self.testProgress)
         
         # File menu
         self.ui.actionselect_folder.triggered.connect(self.choose_directory)
-
+        
+        
         
     # ------------------------------------------------------------------
     # class methods
     # ------------------------------------------------------------------
+
+
+    # ------------------------------------------------------------------
+    # set the animation frames per second
+    def set_framerate(self):
+        self.fps = self.ui.fpsSlider.value()
+        self.ui.fpsLabel.setText(str(self.fps) + " fps")
+        
+    # ------------------------------------------------------------------
+    # create an animation of the current days images
+    #  animation is written to the current imput_dir
+    def animate(self):
+        filecount = len(os.listdir(self.input_dir))
+        self.percentstep = 100/(filecount * 2)
+        self.percentdone = 0
+        
+        a=1
+        have_images = False
+        for file in sorted(os.listdir(self.input_dir)):
+            in_file = self.input_dir + file
+            print(in_file)
+            
+            if fnmatch.fnmatch(file, '*.jpg'):
+                self.percentdone = self.percentdone + self.percentstep
+                self.ui.progress.setValue(self.percentdone)
+                have_images = True
+                out_file = self.temp_dir + str(a).zfill(4) + '.jpg'
+                print(out_file)
+                shutil.copy(in_file, out_file)
+                a = a + 1
+            
+        if have_images:
+            # first remove old animation
+            if os.path.exists(self.input_dir + "today-" + str(self.fps) + "_fps.mp4"):
+                os.remove(self.input_dir + "today-" + str(self.fps) + "_fps.mp4")
+                
+            command = f"ffmpeg -framerate {self.fps} -i {self.temp_dir}%04d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p {self.input_dir}today-{self.fps}_fps.mp4"
+            print(command)
+            subprocess.call(command,shell=True)
+            
+            # clean up
+            for file in os.listdir(self.temp_dir):
+                if re.search('.jpg', file):
+                    os.remove(os.path.join(self.temp_dir, file))
+                    self.percentdone = self.percentdone + self.percentstep
+                    self.ui.progress.setValue(self.percentdone)
+            #os.remove(self.temp_dir + "*.jpg")
+        
+        #self.show_done()
+        self.percentdone = 0
+        self.ui.progress.setValue(self.percentdone)
 
     # ------------------------------------------------------------------
     # check in case the directory we are in was deleted
@@ -153,8 +202,14 @@ class MyApp(QMainWindow, Ui_MainWindow):
         changed = 0
         self.ui.filecount.setText('Deleting marked images...')
         self.ui.filecount.update()
+        # show progress
+        self.percentstep = 100/len(self.data) * 3
+        
         # go thru the data/log and delete any files marked for deletion
         for item in self.data:
+            # show progress
+            self.percentdone = self.percentdone + self.percentstep
+            self.ui.progress.setValue(self.percentdone)
             if item['status'] == 'delete':
                 changed = 1
                 path = item['folder']
@@ -168,11 +223,18 @@ class MyApp(QMainWindow, Ui_MainWindow):
                         print("No such file.")
                 # add deleted files to the delete list
                 self.dataupdate.append({'folder':path,  "file":file, "status":'delete'})
+                
         # go thru the delete list and remove the entries from the data/log
+        # show progress
+        self.percentdone = 0
+        self.percentstep = 100/len(self.dataupdate)
         for item in self.dataupdate:
             thisfolder = item['folder']
             self.ui.filecount.setText('Checking folder: ' + thisfolder)
             self.ui.filecount.update()
+            # show progress
+            self.percentdone = self.percentdone + self.percentstep
+            self.ui.progress.setValue(self.percentdone)
             # check the folder
             if os.path.isdir(thisfolder):
                 list = os.listdir(thisfolder)
@@ -186,7 +248,13 @@ class MyApp(QMainWindow, Ui_MainWindow):
         if changed == 1:
             self.ui.filecount.setText('Updating data file...')
             self.ui.filecount.update()
+            # show progress
+            self.percentdone = 0
+            self.percentstep = 100/len(self.dataupdate)
             for updates in self.dataupdate:
+                # show progress
+                self.percentdone = self.percentdone + self.percentstep
+                self.ui.progress.setValue(self.percentdone)
                 try:
                     self.data.remove({'folder':updates['folder'],  'file':updates['file'],  'status':updates['status']})
                 except:
@@ -196,6 +264,9 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.ui.filecount.setText('Done.')
         self.adjustinput_dir()
         self.openfolder()
+        # show progress
+        self.percentdone = 0
+        self.ui.progress.setValue(self.percentdone)
             
     # ------------------------------------------------------------------
     # move to the previous folder in the parent directory
@@ -226,7 +297,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.updatelist()
         self.savedatafile()
         self.changes = 1
- 
+
+    
     # ------------------------------------------------------------------
     # toggle the current image
     def keepimage(self):
@@ -289,9 +361,13 @@ class MyApp(QMainWindow, Ui_MainWindow):
         if len(list) == 0:
             self.ui.filelistWidget.addItem("Empty Folder")
         else:
+            self.percentstep = 100/len(os.listdir(self.input_dir))
             for filename in sorted(os.listdir(self.input_dir)):
                 status = self.defaultstatus
                 newfile = 1
+                # show progress
+                self.percentdone = self.percentdone + self.percentstep
+                self.ui.progress.setValue(self.percentdone)
                 if ('.jpg' in filename):
                     for item in self.data:
                         if self.input_dir == item["folder"]:
@@ -309,6 +385,9 @@ class MyApp(QMainWindow, Ui_MainWindow):
             if self.number_of_images > 0:
                 self.updatefilecount()
                 self.updateimage()
+                
+            self.percentdone = 0
+            self.ui.progress.setValue(self.percentdone)
             
     # ------------------------------------------------------------------
     # update the display of current image number and cound of images in the selected folder
